05e16bab785878cb0bd14859b5ea6e7377b71450
User <User@DESKTOP-5PSJK0D>
2024-06-16 16:16:09 +0300

    Intro

---
+++ IntroductionToDOTNET/IntroductionToDOTNET/Program.cs
@@ -0,0 +1,51 @@
+﻿//#define CLASS_CONSOLE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToDOTNET
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+#if CLASS_CONSOLE
+            Console.Title = "Введение в .NET";
+            Console.WriteLine("\t\t\tHello .NET");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.CursorLeft = 32;
+            Console.CursorTop = 11;
+            Console.Beep(137, 2000);
+            Console.Clear();
+            //Console.SetCursorPosition(22, 11);
+            Console.WriteLine("Привет .NET");
+            Console.ResetColor(); 
+#endif
+			Console.Write("Введите Ваше имя: ");
+			string first_name = Console.ReadLine();
+
+			Console.Write("Введите Вашу фамилию: ");
+			string last_name = Console.ReadLine();
+
+			Console.Write("Введите Ваш возраст:");
+			int age = Convert.ToInt32(Console.ReadLine());
+
+			#region ConsoleWriteRegion
+			/*Console.Write(first_name + " ");
+                Console.Write(last_name + " ");
+                Console.Write(age);
+                Console.WriteLine();*/    //cout << endl; 
+			#endregion
+
+			//Console.WriteLine(last_name + " " + first_name + " " + age);    //Strings concatenation
+
+			//Console.WriteLine(string.Format("{0} {1} {2}", last_name, first_name, age));    //String formatting
+
+			//Console.WriteLine($"{last_name} {first_name} {age}");	//Strings interpolation
+			
+		}
+	}
+}
---
+++ IntroductionToDOTNET/ReadMe.txt
@@ -0,0 +1,6 @@
+https://github.com/okovtun/PD_311_CS.git
+
+
+1. https://github.com/okovtun/BV_011/blob/master/ControlStructures/Geometria/Task/Geometria1.txt
+2. https://github.com/okovtun/BV_011/blob/master/ControlStructures/Geometria/Task/ChessBoard.jpg
+3. https://github.com/okovtun/BV_011/blob/master/ControlStructures/Geometria/Task/HardChess.jpg
