5020977f883bc5cf3d4c75fe599e581196947d70
User <User@DESKTOP-OSEFFHS>
2024-06-19 15:15:52 +0300

    Calc 1 DONE

---
+++ IntroductionToDOTNET/Calc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите простое арифметическое выражение: ");
+			string expression = Console.ReadLine();
+			expression = expression.Replace(',', '.');
+			Console.WriteLine(expression);
+			String[] numbers = expression.Split('+', '-', '*', '/');
+			//for (int i = 0; i < numbers.Length; i++)Console.Write(numbers[i] + "\t");
+			double a = Convert.ToDouble(numbers[0]);
+			double b = Convert.ToDouble(numbers[1]);
+			char s = expression[expression.IndexOfAny(new char[]{ '+', '-', '*', '/'})];
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+			Console.WriteLine(s);
+			switch (s)
+			{
+				case '+': Console.WriteLine($"{a} + {b} = {a+b}"); break;
+				case '-': Console.WriteLine($"{a} - {b} = {a-b}"); break;
+				case '*': Console.WriteLine($"{a} * {b} = {a*b}"); break;
+				case '/': Console.WriteLine($"{a} / {b} = {a/b}"); break;
+			}
+			Main(args);	//Рекурсивный вызов функции Main(), при завершении программы она заново звпускается.
+		}
+	}
+}
--- IntroductionToDOTNET/DataTypes/Program.cs
+++ IntroductionToDOTNET/DataTypes/Program.cs
@@ -1,4 +1,5 @@
-﻿//#deine INTEGRAL_TYPES
+﻿#define INTEGRAL_TYPES
+//#define FLOATING_TYPES
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,31 +15,49 @@ namespace DataTypes
 		{
 #if INTEGRAL_TYPES
 			Console.WriteLine("ushort");
+			ushort _ushort = 0;
+			Console.WriteLine($"Класс-обвертка:\t{_ushort.GetType()}");
 			Console.WriteLine($"Занимает {sizeof(ushort)} Байта");
 			Console.WriteLine($"Диапазон принимаемых значений: {ushort.MinValue} ... {ushort.MaxValue}");
 			Console.WriteLine(delimiter);
 
 			Console.WriteLine("short");
+			short _short = 0;
+			Console.WriteLine($"Класс-обвертка:\t{_short.GetType()}");
 			Console.WriteLine($"Занимает {sizeof(short)} Байта");
 			Console.WriteLine($"Диапазон принимаемых значений: {short.MinValue} ... {short.MaxValue}");
 			Console.WriteLine(delimiter);
 
 			Console.WriteLine("uint");
+			uint _uint = 0;
+			Console.WriteLine($"Класс-обвертка:\t{_uint.GetType()}");
 			Console.WriteLine($"Занимает {sizeof(uint)} Байта");
 			Console.WriteLine($"Диапазон принимаемых значений: {uint.MinValue} ... {uint.MaxValue}");
 			Console.WriteLine(delimiter);
 
-			Console.WriteLine("uint");
+			Console.WriteLine("int");
+			int _int = 0;
+			Console.WriteLine($"Класс-обвертка:\t{_int.GetType()}");
 			Console.WriteLine($"Занимает {sizeof(int)} Байта");
 			Console.WriteLine($"Диапазон принимаемых значений: {int.MinValue} ... {int.MaxValue}");
 			Console.WriteLine(delimiter);
 
 			Console.WriteLine("ulong");
+			ulong _ulong = 0;
+			Console.WriteLine($"Класс-обвертка:\t{_ulong.GetType()}");
 			Console.WriteLine($"Занимает {sizeof(ulong)} Байта");
 			Console.WriteLine($"Диапазон принимаемых значений: {ulong.MinValue} ... {ulong.MaxValue}");
-			Console.WriteLine(delimiter); 
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine("long");
+			long _long = 0;
+			Console.WriteLine($"Класс-обвертка:\t{_long.GetType()}");
+			Console.WriteLine($"Занимает {sizeof(long)} Байта");
+			Console.WriteLine($"Диапазон принимаемых значений: {long.MinValue} ... {long.MaxValue}");
+			Console.WriteLine(delimiter);
 #endif
 
+#if FLOATING_TYPES
 			//	float, double, decimal
 
 			Console.WriteLine("float");
@@ -54,8 +73,15 @@ namespace DataTypes
 			Console.WriteLine("decimal");
 			Console.WriteLine($"Занимает {sizeof(decimal)} Байта");
 			Console.WriteLine($"Диапазон принимаемых значений: {decimal.MinValue} ... {decimal.MaxValue}");
-			Console.WriteLine(delimiter);
+			Console.WriteLine(delimiter); 
+#endif
+			long a = 2;
+			ulong b = 3;
+			//a = int(b);	//Syntax error
+			a = (int)b;
+			//Console.WriteLine((a + b).GetType());
 
+			//Convert
 		}
 	}
 }
